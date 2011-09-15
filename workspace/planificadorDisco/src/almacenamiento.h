bba3c356a460f198bfa434d4ef2a082fd6aca6e5
Matias Garcia Isaia <mgarciaisaia@users.noreply.github.com>
2011-09-15 01:45:32 +0000

    Bozquejo inicial de la interface de almacenamiento

---
+++ workspace/planificadorDisco/src/almacenamiento.h
@@ -0,0 +1,16 @@
+#ifndef ALMACENAMIENTO_H_
+#define ALMACENAMIENTO_H_
+
+/*
+ * En almacenamiento.h debieramos poner la interfaz comun de los procesos que
+ * tratan con el almacenamiento fisico de los datos, es decir, el RAID y el
+ * disco
+ */
+/*
+ * TODO: definir los datos del struct
+ */
+struct datos_sector {
+
+};
+
+#endif /* ALMACENAMIENTO_H_ */
