ac8118cbf6ec4895945958caf81f17bbba283c53
Matias Garcia Isaia <mgarciaisaia@users.noreply.github.com>
2011-09-30 20:37:02 +0000

    Planificador: abre archivo y se supone que lee sectores

--- workspace/planificadorDisco/src/planificadorDisco.c
+++ workspace/planificadorDisco/src/planificadorDisco.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 #include "almacenamiento.h"
 
+typedef char* string;
+
 struct configuracion {
-	char *rutaAlDisco;
+	string rutaAlDisco;
 	int archivoDisco;
 };
 
-int main(int argc, char *argv[]) {
-	struct configuracion configuracion;
-	configurar(argc, argv, configuracion);
-	abrirArchivoDisco(configuracion);
-	return EXIT_SUCCESS;
+
+void modoDeUso(string executable) {
+	printf("*** Modo de uso ***\n");
+	printf("\tplanificador PATH_AL_DISCO\n");
+	printf("\nPor ejemplo:\n");
+	printf("$ %s fat32.disk\n", executable);
+}
+
+void salirConModoDeUso(int argc, string argv[]) {
+	modoDeUso(argv[0]);
+	perror("mala cantidad de parametros");
+	exit(EXIT_FAILURE);
 }
 
 /**
@@ -20,36 +32,21 @@ int main(int argc, char *argv[]) {
  *
  * Deja todo preparado para poder operar
  */
-void configurar(int argc, char *argv[], struct configuracion configuracion) {
+void configurar(int argc, string argv[], struct configuracion *configuracion) {
 	if(argc != 2) {
-		exitWithUsage(argc, argv);
+		salirConModoDeUso(argc, argv);
 	}
 
-	// Ponele que con esto copiamos el primer parametro de la aplicacion
-	// (el path al archivo del disco) en el struct de configuracion
-	strcpy(configuracion.rutaAlDisco, argv[1]);
-
+	(*configuracion).rutaAlDisco = argv[1];
 	// TODO: aca deberia leer el planificador.ini y seguir leyendo configuracion
 }
 
-void salirConModoDeUso(int argc, char *argv[]) {
-	modoDeUso();
-	perror("mala cantidad de parametros - " + argc);
-	exit(EXIT_FAILURE);
+void abrirArchivoDisco(struct configuracion *configuracion) {
+	(*configuracion).archivoDisco = open((*configuracion).rutaAlDisco, O_RDWR);
+	perror("abrirARchivo");
 }
 
-void modoDeUso(void) {
-	printf("*** Modo de uso ***\n");
-	printf("\tplanificador PATH_AL_DISCO\n");
-	printf("\nPor ejemplo:\n");
-	printf("$ ./planificador fat32.disk\n");
-}
-
-void abrirArchivoDisco(struct configuracion configuracion) {
-	configuracion.archivoDisco = open(configuracion.rutaAlDisco, NULL);
-}
-
-void leerSector(int32_t numeroSector, fat32_sector data, struct configuracion configuracion) {
+void leerSector(int32_t numeroSector, fat32_sector *data, struct configuracion *configuracion) {
 	/*
 	 * FIXME: esta linea que sigue ESTA MAL. No tiene que leer "sizeof fat32_sector"
 	 *
@@ -62,11 +59,34 @@ void leerSector(int32_t numeroSector, fat32_sector data, struct configuracion co
 	 * Osea, ACA va la cache :) (salvo que en el campus virtual esten hablando de lo equivocados que estamos,
 	 * en cuyo caso no tengo idea que tendriamos que hacer)
 	 */
-	data = mmap((caddr_t) 0, sizeof fat32_sector, PROT_READ, MAP_SHARED, configuracion.archivoDisco, getpagesize());
-
-	// FIXME: implementar
+	char *dataFile = mmap((caddr_t) 0, getpagesize(), PROT_READ, MAP_SHARED, (*configuracion).archivoDisco, 0);
+	if(dataFile == (caddr_t) -1) {
+		perror("mmap de read sector");
+		exit(1);
+	}
+	memcpy(data, dataFile + (numeroSector * 512), 512);
+	munmap(dataFile, 512);
 }
 
 void escribirSector(int32_t numeroSector, fat32_sector data, struct configuracion configuracion) {
-	// FIXME: implementar
+	char *dataFile = mmap((caddr_t) 0, getpagesize(), PROT_WRITE, MAP_SHARED, configuracion.archivoDisco, 0);
+	if(dataFile == (caddr_t) -1) {
+		perror("mmap de write sector");
+		exit(1);
+	}
+	memcpy(dataFile + (numeroSector * 512), data, 512);
+	munmap(dataFile, 512);
+}
+
+int main(int argc, string *argv) {
+	struct configuracion configuracion;
+	printf("argv[0]: %p\n%s\n", argv, *argv);
+	configurar(argc, argv, &configuracion);
+	//printf("Bleh: %s\n%s", argv[1], configuracion.rutaAlDisco);
+	//exit(0);
+	abrirArchivoDisco(&configuracion);
+    fat32_sector data;
+	leerSector(0, &data, &configuracion);
+	printf("Data: %s", data);
+	return EXIT_SUCCESS;
 }
